e0be62c67124e852dfcb698d85cd796ab1f45912
Aditya <adityalimaye25@gmail.com>
2019-08-30 14:49:02 +0530

    Improvised Login UI and Register UI Improvised Landing Page Constraints

--- app/src/main/java/com/skar/vote/ActivityLogin.java
+++ app/src/main/java/com/skar/vote/ActivityLogin.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -38,8 +37,7 @@ public class ActivityLogin extends AppCompatActivity {
 
         requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
         getSupportActionBar().hide(); // hide the title bar
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+
         setContentView(R.layout.activity_login);
 
         findViews();
--- app/src/main/java/com/skar/vote/ActivityRegister.java
+++ app/src/main/java/com/skar/vote/ActivityRegister.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -41,8 +40,7 @@ public class ActivityRegister extends AppCompatActivity {
 
         requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
         getSupportActionBar().hide(); // hide the title bar
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+
         setContentView(R.layout.activity_register);
 
         firebaseAuth = FirebaseAuth.getInstance();
